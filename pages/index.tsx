aff340a03aab7945c00d729bd53c1b54a21dc08b
heeheepark <pjh9641@gmail.com>
2023-09-19 11:02:02 +0900

    feat: Map Marker 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Header from '@/components/common/Header';
 import MapScene from '@/components/home/MapScene';
+import useInfo from '@/hooks/useInfo';
 import { Info } from '@/types/info';
 import { useEffect } from 'react';
 interface IProps {
@@ -7,9 +8,15 @@ interface IProps {
 }
 
 export default function Home({ infos }: IProps) {
+  // 위·경도에 대한 정보(.json) 및 naver의 map 객체를 전역으로 참조해서
+  // SWR 상태관리 ()
+  const { initializeInfos } = useInfo();
+  // 최초 mount가 되면(html이 출력이 되면) props를 출력해본다.
   useEffect(() => {
     console.log(infos);
-  }, [infos]);
+    initializeInfos(infos);
+    // 새로운 정보가 들어온다면 다시 랜더링 해주기 위해서
+  }, [initializeInfos, infos]);
   return (
     <>
       <Header />
@@ -21,9 +28,11 @@ export default function Home({ infos }: IProps) {
 // pre-rendering을 해서 SSG 생성
 export async function getStaticProps() {
   // public에 있는 info.json 가져오기
+  // props 전달
   const infos = (await import('@/public/info.json')).default;
   return {
     props: { infos },
-    revalidate: 3600,
+    // 일정한 시간이 지나면 데이터를 다시 가져와서 pre-rendering 한다.
+    // revalidate: 3600,
   };
 }
